39f9870e5ef9a3cfd6e887583a9ff6b328d1d0de
Ekaterina Dontsova <ekaterina.dontsova@gmail.com>
2016-12-13 22:17:39 -0800

    country data added

--- README.md
+++ README.md
@@ -8,5 +8,7 @@ Design - explain any design choices you made including changes to the visualizat
 Feedback:
 
 Resources:
-	-https://www.udacity.com/course/data-visualization-and-d3js--ud507
-	-http://www.isaaa.org/gmapprovaldatabase/default.asp
+	-https://www.udacity.com/course/data-visualization-and-d3js--ud507   # d3 course and examples
+	-http://www.isaaa.org/gmapprovaldatabase/default.asp  # gmo approval data
+	-http://gothos.info/2009/02/centroids-for-countries/  # contries coordinates
+	-http://data.okfn.org/data/core/country-list          # countries codes
--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 function draw(geo_data) {
 	"use strict";
+	
+	var pathToData = "./data/data.tsv";
+	var pathToCountryCenroids = "./data/country_centroids_all.csv";
+	var pathToCountryCodes = "./data/country_codes.csv";
+	
+	// map 
+	
 	var margin = 75,
 		width = 1400 - margin,
 		height = 600 - margin;
@@ -10,4 +17,44 @@ function draw(geo_data) {
 		.attr("height", height + margin)
 		.append('g')
 		.attr('class', 'map');
+	
+	var projection = d3.geo.mercator()
+                     .scale(150)
+                     .translate( [width / 2, height / 1.5]);
+
+	var path = d3.geo.path().projection(projection);
+
+	var map = svg.selectAll('path')
+				 .data(geo_data.features)
+				 .enter()
+				 .append('path')
+				 .attr('d', path)
+				 .style('fill', '#f6f8e0')
+				 .style('stroke', '#3c4b52')
+				 .style('stroke-width', 0.5);
+	
+	// data
+	
+	
+	
+	d3.tsv(pathToData, function(erEvents, dEvents) {
+		//console.log("erEvents: " + erEvents);
+		/*for(var key in dEvents[0]){
+			console.log(key);
+			console.log(dEvents[0][key]);
+		}*/
+		d3.csv(pathToCountryCodes, function(erCodes, dCodes) {
+		 // console.log("erCodes " + erCodes);
+		 // console.log("dCodes: " + dCodes[0]);
+		  d3.csv(pathToCountryCenroids, function(erCentroids, dCentroids) {
+			//console.log("erCentroids: " + erCentroids);
+			//console.log("dCentroids: " + dCentroids[0]);
+			for(var key in dEvents[0]){
+				console.log(key);
+				console.log(dEvents[0][key]);
+			}
+			// в центре каждой страны - иконки одобренных культур (рисунок - в зависимости от crop), размер - в зависимости от количетсва видов crop
+		  });
+		});
+	});
 };
